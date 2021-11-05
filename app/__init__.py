ae4b5261fcedf000ff50219a24bf5e669f8bbcad
kipsang01 <enock.kipsang@student.moringaschool.com>
2021-11-05 14:12:27 +0300

    Add inital commit to run flask app

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+
+
+def create_app():
+    app = Flask(__name__)
+    
+    #Registering blueprints
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint,url_prefix='/')
+    
+    # from .auth import auth as auth_blueprint
+    # app.register_blueprint(auth_blueprint,url_prefix = '/')
+    
+    return app
